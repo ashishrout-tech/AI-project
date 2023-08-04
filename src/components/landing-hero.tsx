3d8077d66916526f161829acae1f4d59c6d619dc
ashishrout-tech <ashishrout655@gmail.com>
2023-08-04 15:16:02 +0530

    Error Typo

--- src/components/landing-hero.tsx
+++ src/components/landing-hero.tsx
@@ -32,7 +32,7 @@ export const LandingHero = () => {
                 Create content uisng AI 10x faster.
             </div>
             <div>
-                <Link href={isSignedIn ? "/dashbboard" : "/sign-up"}>
+                <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
                     <Button variant="premium" className="md:text-lg p-4 md:p-6 rounded-full font-semibold">
                         Start Generating for Free
                     </Button>
